1810fa6a74b665cc9499a33742e2f696ae97da4d
hanjala <shihab2305341402@diu.edu.bd>
2025-07-27 22:43:35 +0600

    calculating missing values

--- Handling Missing Values/intro.py
+++ Handling Missing Values/intro.py
@@ -1,8 +1,35 @@
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('NFL_play_v4.csv')
 
 print(data.head())
 print("--------------------")
 
+
 print(data.columns)
+print("--------------------")
+
+np.random.seed(0)
+
+
+#There are many mnissing data points.How many?
+missing_values = data.isnull().sum()
+print(missing_values[0:10])
+print("--------------------")
+
+
+#how many total missin values do we have in percentage?
+import numpy as np
+import pandas as pd
+
+# Assuming 'data' is your DataFrame
+total_cells = np.prod(data.shape)
+print(total_cells)
+
+total_missing = missing_values.sum()
+print(total_missing)
+
+
+missing_percentage = (total_missing / total_cells) * 100
+print(f"{missing_percentage:.2f}%")
