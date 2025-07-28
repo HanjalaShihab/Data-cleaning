0148a11aa0633ddbf176bd8ad75cae5bf501f30f
hanjala <shihab2305341402@diu.edu.bd>
2025-07-28 21:31:58 +0600

    parsing dates

--- Parsing Dates/parse_date.py
+++ Parsing Dates/parse_date.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import datetime
 
+
 landslides = pd.read_csv('Parsing Dates/catalog.csv')
 
 print(landslides.head())
@@ -21,3 +22,28 @@ landslides['date_parsed'] = pd.to_datetime(landslides['date'], format = "%m/%d/%
 print(landslides.columns)
 print()
 print(landslides['date_parsed'].head())
+
+
+print(landslides.head())  #containign both the date and date_parsed column
+
+landslides.drop(columns=['date'], inplace= True)    #dropped the date column
+print(landslides.head())
+
+
+#reordering the columns as the date_parsed column added to the last-
+cols = ['date_parsed'] + [cols for cols in landslides.columns if cols != 'date_parsed']
+landslides = landslides[cols]
+print(landslides.head())
+
+
+#Select the day of the month:
+day_of_month = landslides['date_parsed'].dt.day
+print(day_of_month.head())
+
+
+x = landslides['date_parsed'].isnull().sum().sum() 
+print(x)
+
+day_of_month = day_of_month.dropna()
+
+sns.displot(day_of_month, kde = False, bins = 31)
