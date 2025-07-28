47fa9bd4fe3c9e22e0708450d484b7368a78a7d4
hanjala <shihab2305341402@diu.edu.bd>
2025-07-28 20:53:58 +0600

    parsing dates

--- Parsing Dates/parse_date.py
+++ Parsing Dates/parse_date.py
@@ -6,3 +6,18 @@ import datetime
 landslides = pd.read_csv('Parsing Dates/catalog.csv')
 
 print(landslides.head())
+
+
+#we will be working with the date column:
+print(landslides['date'].head())
+
+#checking the datatype of the date column:
+print("Datatype of the date column is -", landslides['date'].dtype)
+
+
+#converting the date column to datetime:
+landslides['date_parsed'] = pd.to_datetime(landslides['date'], format = "%m/%d/%y")
+
+print(landslides.columns)
+print()
+print(landslides['date_parsed'].head())
